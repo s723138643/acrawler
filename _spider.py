7ec17e3d7ad66fcdac736d2039acc85428afce0c
s723138643 <s723138643@gmail.com>
2016-12-08 11:12:40 +0800

    change spider.run logic

--- _spider.py
+++ _spider.py
@@ -62,43 +62,56 @@ class BaseSpider(AbstractSpider):
         for url in cls.start_urls:
             yield Request(url)
 
+    async def _fetch(self, request):
+        if not request.fetcher:
+            fetcher = self.fetch
+        else:
+            fetcher = getattr(self, request.fetcher)
+        response = await fetcher(request)
+        if response and (not response.request):
+            # to make sure raw request in result
+            response.request = request
+        return response
+
+    async def _parse(self, response):
+        def do_parse(r):
+            if not r.request.parser:
+                parser = self.parse
+            else:
+                parser = getattr(self, r.request.parser)
+            return parser(r)
+
+        if isinstance(response, Response):
+            result = do_parse(response)
+            if result:
+                await self.send_result(result)
+        elif isinstance(response, Request):
+            await self.send_result(response)
+        else:
+            raise TypeError('cannot parse {} object'.format(type(response)))
+
     async def run(self):
         await self._engine.register(self)   # register spider to engine first
         while True:
             task = await self._tasks.get()
             if isinstance(task, Stop):
-                logger.info('<spider-{}> quit, {}.'
-                            .format(self._name, task.msg))
+                logger.info('<spider-{}> quit, {}.'.format(self._name,
+                                                           task.msg))
                 break
-            logger.info('<spider-{}> got task:<{}>'
-                        .format(self._name, task.url))
+
             try:
-                if not task.fetcher:
-                    fetcher = self.fetch
-                else:
-                    fetcher = getattr(self, task.fetcher)
-                response = await fetcher(task)
-            except AttributeError as e:   # Can't find fether
+                logger.info('<spider-{}> got task: {}'.format(self._name,
+                                                              task.url))
+                response = await self._fetch(task)
+                if response:
+                    await self._parse(response)
+                    logger.info('<spider-{}> task: {} done!'
+                                .format(self._name, task.url))
+            except AttributeError as e:   # Can't find fether or parser
                 raise
             except Exception as e:
                 logger.exception(e)
                 continue
-            else:
-                if not response:
-                    continue
-                elif isinstance(response, Request):
-                    await self.send_result(response)
-                elif isinstance(response, Response):
-                    if not task.parser:
-                        parser = self.parse
-                    else:
-                        parser = getattr(self, task.parser)
-                    result = parser(response)
-                    if result:
-                        await self.send_result(result)
-                else:
-                    raise TypeError('unkown Type:{},'
-                                    'except a Request or Response')
             finally:
                 # register spider to engine again
                 await self._engine.register(self)
