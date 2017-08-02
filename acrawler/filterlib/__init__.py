7ae38be91083c94e8ddee219b5ffb55b0ddd3001
s723138643 <s723138643@gmail.com>
2017-08-02 11:06:30 +0800

    disable blumefilter

--- acrawler/filterlib/__init__.py
+++ acrawler/filterlib/__init__.py
@@ -1,8 +1,8 @@
 from .basefilter import BaseFilter
-from .blumefilter import BlumeFilter
+#from .blumefilter import BlumeFilter
 from .dbfilter import SQLiteFilter
-from .mixedfilter import MixedFilter
+#from .mixedfilter import MixedFilter
 from .memfilter import MemFilter
 
 
-__all__ = (BaseFilter, MemFilter, BlumeFilter, SQLiteFilter, MixedFilter)
+__all__ = (BaseFilter, MemFilter, SQLiteFilter)
